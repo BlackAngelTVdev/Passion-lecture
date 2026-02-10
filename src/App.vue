e627f815dba7ffdc2691c04e2205889013ed81f6
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2026-02-10 16:27:40 +0100

    Feat(css): fichier de base
    [15][DONE]

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,38 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import api from '@/services/api';
+import { ref, onMounted, computed } from 'vue'
+import api from '@/services/api'
+import '@/assets/css/home.css'
+import '@/assets/css/app.css'
+const books = ref([])
+const error = ref(null)
 
-const books = ref([]);
-const error = ref(null);
+const latestBooks = computed(() => {
+  return books.value.slice(-5).reverse()
+})
 
 onMounted(async () => {
   try {
-    books.value = await api.getBooks();
+    books.value = await api.getBooks()
   } catch (err) {
-    error.value = "Impossible de charger les données.";
-    console.error(err);
+    error.value = 'Impossible de charger les données.'
+    console.error(err)
   }
-});
+})
 </script>
 
 <template>
-  <div class="p-4">
-    <h2>Ma Liste de Livres</h2>
-    
-    <p v-if="error" style="color: red;">{{ error }}</p>
+  <div class="container">
+    <h2 class="main-title">Un endroit pour vos livres</h2>
 
-    <div v-else class="grid">
-      <div v-for="book in books" :key="book.id" class="card">
-        <h3>{{ book.title }}</h3>
-        <p>Auteur : {{ book.author }}</p>
-        <img :src="book.image" :alt="book.title" width="100">
+    <p v-if="error" class="error">{{ error }}</p>
+
+    <div v-else class="book-grid">
+      <div v-for="book in latestBooks" :key="book.id" class="book-card">
+        <img :src="`${book.image}`" :alt="book.title" class="book-image" />
+        <div class="book-info">
+          <span class="title-author">{{ book.title }} | {{ book.author }}</span>
+          <span class="user-label">user</span>
+        </div>
       </div>
     </div>
   </div>
