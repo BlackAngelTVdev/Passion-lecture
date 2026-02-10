7cf4e68d7cf9229e4fdebcba8251070db58f90c7
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2026-02-10 16:03:15 +0100

    Fix(url): images
    [15][DONE]

--- src/services/api.js
+++ src/services/api.js
@@ -7,24 +7,5 @@ export default {
     const response = await fetch(`${BASE_URL}/books`);
     if (!response.ok) throw new Error('Erreur réseau');
     return await response.json();
-  },
-
-  // Récupérer un livre par ID
-  async getBook(id) {
-    const response = await fetch(`${BASE_URL}/books/${id}`);
-    if (!response.ok) throw new Error('Livre introuvable');
-    return await response.json();
-  },
-
-  // Simuler un POST (ajout)
-  async addBook(bookData) {
-    const response = await fetch(`${BASE_URL}/books`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(bookData),
-    });
-    return await response.json();
   }
 };
