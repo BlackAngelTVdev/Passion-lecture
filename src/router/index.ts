9ad4284168128d421f5e52442f2e76a2f6cd62ab
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-02-11 08:47:51 +0100

    feat(index) : création du index pour home et livre
    [15min][DONE]

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+        {
+      path: '/',
+      name: 'home',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../pages/home.vue'),
+    },
+    {
+      path: '/Livres',
+      name: 'Livres',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../pages/liste.vue'),
+    },
+  ],
 })
 
 export default router
