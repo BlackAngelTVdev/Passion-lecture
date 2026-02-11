f445c86f9cff6cea66b9769828858c2231a40ff0
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2026-02-11 08:15:44 +0100

    Feat(DB): add user id  for all books
    [15][DONE]
    permet de savoir qui a publié le livre

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@ import '@/assets/css/home.css'
 import '@/assets/css/app.css'
 import TheHeader from './components/TheHeader.vue'
 import TheFooter from './components/TheFooter.vue'
+
 const books = ref([])
+const users = ref([]) // 1. On crée la ref pour stocker les users
 const error = ref(null)
 
 const latestBooks = computed(() => {
@@ -14,12 +16,21 @@ const latestBooks = computed(() => {
 
 onMounted(async () => {
   try {
+    // 2. On récupère les deux listes
     books.value = await api.getBooks()
+    users.value = await api.getUsers()
   } catch (err) {
     error.value = 'Impossible de charger les données.'
     console.error(err)
   }
 })
+
+// 3. La fonction de recherche qui utilise users.value
+const getUserName = (userId) => {
+  if (!users.value.length) return 'Chargement...'
+  const userFound = users.value.find((u) => u.id === userId)
+  return userFound ? userFound.username : 'Inconnu'
+}
 </script>
 
 <template>
@@ -35,9 +46,10 @@ onMounted(async () => {
         <div v-else class="book-grid">
           <div v-for="book in latestBooks" :key="book.id" class="book-card">
             <img :src="book.image" :alt="book.title" class="book-image" />
+
             <div class="book-info">
               <span class="title-author">{{ book.title }} | {{ book.author }}</span>
-              <span class="user-label">user</span>
+              <span class="user-label">Par : {{ getUserName(book.userId) }}</span>
             </div>
           </div>
         </div>
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,16 @@
 // Ta base URL My JSON Server
-const BASE_URL = 'https://my-json-server.typicode.com/BlackAngelTVdev/Passion-lecture/';
+const BASE_URL = 'https://my-json-server.typicode.com/BlackAngelTVdev/Passion-lecture/'
 
 export default {
   // Récupérer tous les livres
   async getBooks() {
-    const response = await fetch(`${BASE_URL}/books`);
-    if (!response.ok) throw new Error('Erreur réseau');
-    return await response.json();
-  }
-};
+    const response = await fetch(`${BASE_URL}/books`)
+    if (!response.ok) throw new Error('Erreur réseau')
+    return await response.json()
+  },
+  async getUsers() {
+    const response = await fetch(`${BASE_URL}/user`)
+    if (!response.ok) throw new Error('Erreur réseau lors de la récupération des users')
+    return await response.json()
+  },
+}
