32993ffd04e1d9ad7bfb0bdb6dd3f1a56b73f1c1
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2026-02-24 13:21:35 +0100

    Fix(email): email on footer
    [11][DONE]

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,25 +3,17 @@ import '@/assets/css/app.css'
 </script>
 
 <template>
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-  />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
   <footer>
     <div class="gauche">
       <img src="/logo.png" alt="Logo Passion Lecture" class="footer-logo" />
       <p>&copy; 2026 - Passion Lecture Tous droits réservés ©</p>
     </div>
     <div class="droite">
-      <a href="mailto:damien@example.com" class="footer-link"
-        >Damien Rochat <i class="fas fa-envelope"></i
-      ></a>
-      <a href="mailto:gianmarco@example.com" class="footer-link"
-        >Gianmarco Ruberti <i class="fas fa-envelope"></i
-      ></a>
-      <a href="mailto:albert@example.com" class="footer-link"
-        >Albert Braimi <i class="fas fa-envelope"></i
-      ></a>
+      <a href="mailto:damien.rochat@eduvaud.ch" class="footer-link">Damien Rochat <i class="fas fa-envelope"></i></a>
+      <a href="mailto:gianmarco.ruberti@eduvaud.ch" class="footer-link">Gianmarco Ruberti <i
+          class="fas fa-envelope"></i></a>
+      <a href="mailto:albert.braimi@eduvaud.ch" class="footer-link">Albert Braimi <i class="fas fa-envelope"></i></a>
     </div>
   </footer>
 </template>
