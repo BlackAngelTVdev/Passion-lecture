2dc80c1c9b5b42f65686d7993630d650c4988124
Damien Rochat <pb17shq@eduvaud.ch>
2026-03-04 08:52:06 +0100

    Fix(duplication): link
    [20][DONE]
    https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -14,6 +14,7 @@ import '@/assets/css/app.css'
       <a href="mailto:gianmarco.ruberti@eduvaud.ch" class="footer-link">Gianmarco Ruberti <i
           class="fas fa-envelope"></i></a>
       <a href="mailto:albert.braimi@eduvaud.ch" class="footer-link">Albert Braimi <i class="fas fa-envelope"></i></a>
+
     </div>
   </footer>
 </template>
